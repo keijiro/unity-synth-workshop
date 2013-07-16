1d0f21289662b5101fb3fb315f8b148cbdaaafc5
Keijiro Takahashi <keijiro@Super-MSX.local>
2013-07-16 17:31:32 +0900

    Brushing up the synthesizer code.

--- Assets/Synthesizer.js
+++ Assets/Synthesizer.js
@@ -2,43 +2,93 @@
 
 var freq = 440.0;
 var semitone = 0;
+var modulation = 0.2;
+var octave = 0;
+
+private var sampleRate = 44100.0;
+private var phase = 0.0;
 
 var attack = 0.1;
 var release = 0.5;
 
-var volume = 0.0;
-var keyDown = false;
+private var envelope = 0.0;
+private var noteOn = false;
 
-private var tick = 0;
+function UpdateSynth() {
+	freq = 440.0 * Mathf.Pow(2.0, (semitone - 9 + octave * 12) / 12.0);
+}
+
+function TickSynth() {
+	if (noteOn) {
+		envelope += 1.0 / (sampleRate * attack);
+	} else {
+		envelope -= 1.0 / (sampleRate * release);
+	}
+	envelope = Mathf.Clamp01(envelope);
+	
+	// Sine wave
+	// return Mathf.Sin(phase * Mathf.PI * 2) * envelope;
+
+	// Sawtooth wave
+	// return (phase * 2 - 1) * envelope;
+
+	// Rectangle wave
+	// return (phase < modulation ? -1 : 1) * envelope;
+	
+	// Triangular wave
+	// return (phase < 0.25 ? phase * 4 : (phase < 0.75 ? 2.0 - phase * 4.0 : phase * 4 - 4)) * envelope;
+	
+	// 4-bit Sine wave
+	// return Mathf.Floor(Mathf.Sin(phase * Mathf.PI * 2) * 8) / 8 * envelope;
+	
+	// FM Synthesis
+	var carrier = Mathf.Sin(phase * Mathf.PI * 2 * 8) * modulation;
+	return Mathf.Sin(phase * Mathf.PI * 2 + carrier) * envelope;
+}
+
+////////////////
+
+function Start() {
+	sampleRate = AudioSettings.outputSampleRate;
+}
+
+function Update() {
+	ProcessKeyboard();
+}
 
 function OnAudioFilterRead(data : float[], channels : int) {
-	freq = 440.0 * Mathf.Pow(2.0, (semitone + 3) / 12.0);
-
+	UpdateSynth();
+	var delta = freq / sampleRate;
     for (var i = 0; i < data.Length; i += 2) {
-    	if (keyDown) {
-    		volume = Mathf.Clamp01(volume + 1.0 / 44100.0 / attack);
-    	} else {
-    		volume = Mathf.Clamp01(volume - 1.0 / 44100.0 / release);
-    	}
-    
-    	tick++;
-    	var value = Mathf.Sin(freq * Mathf.PI * 2 * 12 * tick / 44100.0);
-    	value = Mathf.Sin(freq * Mathf.PI * 2 * tick / 44100.0 + 0.3 * value) * volume * 0.9;
-        data[i  ] = value;
-        data[i+1] = value;
+    	phase += delta;
+    	phase -= Mathf.Floor(phase);
+        data[i] = data[i + 1] = TickSynth();
     }
 }
 
-private var keys = ['a', 'w', 's', 'e', 'd', 'f', 't', 'g', 'y', 'h', 'u', 'j', 'k', 'o', 'l', 'p', ';'];
+////////////////
 
-function Update() {
-	keyDown = false;
-	for (var i = 0; i < keys.Length; i++) {
-		if (Input.GetKey(keys[i])) {
-			if (semitone != i) volume = 0.0;
+private var keyboardArray = [
+	'a', 'w', 's', 'e', 'd',
+	'f', 't', 'g', 'y', 'h', 'u', 'j', 
+	'k', 'o', 'l', 'p', ';'
+];
+
+private var lastPressedKey = '';
+
+function ProcessKeyboard() {
+	for (var i = 0; i < keyboardArray.Length; i++) {
+		var key = keyboardArray[i];
+		if (Input.GetKeyDown(key)) {
 			semitone = i;
-			keyDown = true;
-			break;
+			lastPressedKey = key;
+			noteOn = true;
+		} else if (Input.GetKeyUp(key) && lastPressedKey == key) {
+			lastPressedKey = '';
+			noteOn = false;
 		}
 	}
+	
+	if (Input.GetKeyDown('z')) octave--;
+	if (Input.GetKeyDown('x')) octave++;
 }
